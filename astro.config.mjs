85a098de31f6499eada83d3b340978fb37debeaf
Domasch <magesthomas@gmail.com>
2023-09-11 18:51:52 +0200

    add styling for the sntax highlighting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
+
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+import tailwindIntegration from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com/',
-  integrations: [mdx(), sitemap(), tailwind()],
-  output: 'server',
-  adapter: vercel(),
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwindIntegration()],
 });
