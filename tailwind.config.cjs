0559aa91b513bc440722d2b2c331cf85e1abe98c
Domasch <magesthomas@gmail.com>
2024-09-12 00:20:03 +0200

    enw design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,19 @@
+
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
+        extend: {
+            fontFamily: {
+                'jetbrains': ['JetBrainsMono', 'monospace']
+            },
+        },
         fontSize: {
             sm: '0.8rem',
             base: '1rem',
             xl: '1.25rem',
         },
-        plugins: [],
-    }
+    },
+    plugins: [],
 }
